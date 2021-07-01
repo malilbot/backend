19dd74337a0f2e34a804c630d2203fea7bbb2351
SkyBlockDev <danielplay@outlook.com>
2021-07-01 10:03:45 +0200

    add image examples

---
+++ createList.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const files = fs.readdirSync(path.join(__dirname, './images'));
+const filess = [];
+const fileMap = {};
+for (const file of files) {
+	const filePath = `https://github.com/malilbot/backend/blob/main/images/${file}`;
+	const name = file.replace('.png', '');
+	filess.push(filePath);
+	fileMap[name] = filePath;
+}
+fs.writeFileSync('filemap.json', JSON.stringify(fileMap));
+fs.writeFileSync('files.json', JSON.stringify(filess));
