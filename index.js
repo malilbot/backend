0981a0655b4dc279b3843fc7bc3ace6402fb6ed1
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-07-16 10:11:49 +0200

    AAAA

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const fastify = require('fastify');
+const fastify = require("fastify");
 const app = fastify();
-const pet = require('pet-pet-gif');
-const asciify = require('asciify-image');
+const pet = require("pet-pet-gif");
+const asciify = require("asciify-image");
 const options = {
-	fit: 'box',
-	width: 64,
-	height: 64,
-	color: false,
+  fit: "box",
+  width: 64,
+  height: 64,
+  color: false,
 };
 
-app.get('/', (req, res) => {
-	res.send('hi');
+app.get("/", (req, res) => {
+  res.send("hi");
 });
 
-app.get('/pet/:url', async function (req, res) {
-	const url = await pet(await req.query.url).then((b) => b.toString('base64'));
-	await res.send(url);
+app.get("/pet/:url", async function (req, res) {
+  const url = await pet(await req.query.url).then((b) => b.toString("base64"));
+  await res.send(url);
 });
 
-app.get('/asciify/:url', async function (req, res) {
-	asciify(req.query.url, options, (e, a) => {
-		return res.json({ out: `${a}` });
-	});
+app.get("/asciify/:url", function (req, res) {
+  return asciify(req.query.url, options, (e, a) => {
+    return res.send(a);
+  });
 });
 
-app.listen(1234, '0.0.0.0');
+app.listen(1234, "0.0.0.0");
